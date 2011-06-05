cc917df297a5d98560f47f798d2ae7cf718ecb1b
cming <cming.xu@photomugs.com>
2011-06-05 17:22:20 +0800

    ddd

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,14 +2,18 @@ class City < Locale
   has_many :areas, :foreign_key=>"parent_id"
 
   def self.opened
-#    where("short_code in (?)", short_codes)
-    [:bj, :tj, :dl,
-     :sh, :hz, :nj,
-     :gz, :sz, :fs,
-     :cd, :xa, :cs
-    ].map do |short_code|
-      City.find_by_short_code(short_code)
-    end.compact
+    #    where("short_code in (?)", short_codes)
+    short_codes =  [:bj, :tj, :dl,
+      :sh, :hz, :nj,
+      :gz, :sz, :fs,
+      :cd, :xa, :cs
+    ]
+#    short_codes.map do |short_code|
+#      City.find_by_short_code(short_code)
+#    end.compact
+ 
+  
+        where("short_code in (?)", short_codes).compact
   end
 
   def to_xml(options = {})
